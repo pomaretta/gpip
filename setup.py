6f32df1f87c50b42782fee78e715424bc0f75965
Carlos Pomares <cpomaresp@gmail.com>
2021-06-17 09:20:08 +0200

    Renamed to GPIP

--- setup.py
+++ setup.py
@@ -5,14 +5,14 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-	name='privatepip',  
+	name='gpip',  
 	version='0.1',
 	author="Carlos Pomares",
 	author_email="cpomaresp@gmail.com",
 	description="A PIP Private Utility Package.",
 	long_description=long_description,
 	long_description_content_type="text/markdown",
-	url="https://github.com/pomaretta/private-pip",
+	url="https://github.com/pomaretta/gpip",
 	package_dir={"": "src"},
     packages=setuptools.find_packages(where="src"),
 	classifiers=[
