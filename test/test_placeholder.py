250a29953760ae5c0de3dd82b36d765f9425f881
Carlos Pomares <cpomaresp@gmail.com>
2021-07-15 09:06:29 +0200

    Added CI Action and placeholder test

---
+++ test/test_placeholder.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+
+# ========================= #
+# TEST PLACEHOLDER          #
+# ========================= #
+
+import unittest
+
+class PlaceHolderTest(unittest.TestCase):
+    
+    def test_placeholder(self):
+        self.assertTrue(True)
